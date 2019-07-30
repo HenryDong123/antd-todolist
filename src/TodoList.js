533fdc06ef3b7753a8ff0da9a1637795de4dcbb3
HenryDong <943082205@qq.com>
2019-07-30 17:11:46 +0800

    组件分离UI组件容器组件，无状态组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,39 +1,25 @@
 import React, {Component} from 'react'
-import {Input, List} from 'antd'
 import './item.css'
 import store from './store'
+import TodoListUi from './TodoListUi'
 import {addListAction, deleteItemAction, getInputChangeAction} from "./store/actionCreate";
-const { Search } = Input;
 class TodoList extends Component{
     constructor(props){
         super(props)
-        // this.state = {
-        //     itemList: [],
-        //     inputValue: '',
-        //     isToggle: false
-        // }
         this.state = store.getState()
         this.storeChange = this.storeChange.bind(this)
         store.subscribe(this.storeChange)
         this.addItem = this.addItem.bind(this)
         this.handleInput = this.handleInput.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
     }
     render() {
-        return (
-            <div>
-                <Search value={this.state.inputValue} onChange={this.handleInput} enterButton="添加" onSearch={this.addItem} placeholder="输入事项" allowClear={true} size='default' style={{width: '300px',margin: '40px auto',display: 'block'}}/>
-                <List
-                    bordered
-                    dataSource={this.state.itemList}
-                    style={{width: '300px', margin: '-20px auto'}}
-                    renderItem={(item, index) => (
-                        <List.Item className={"item" + (this.state.isToggle? ' active': '')} onClick={this.deleteItem.bind(this, index)}>
-                            {item}
-                        </List.Item>
-                    )}
-                />
-            </div>
-        )
+        return <TodoListUi
+                    inputValue={this.state.inputValue}
+                    itemList={this.state.itemList}
+                    handleInput={this.handleInput}
+                    addItem={this.addItem}
+                    deleteItem={this.deleteItem} />
     }
     storeChange(){
         this.setState(store.getState())
@@ -43,17 +29,11 @@ class TodoList extends Component{
         const action = getInputChangeAction(val)
         store.dispatch(action)
     }
-    addItem(e){
+    addItem(){
        const action = addListAction()
        store.dispatch(action)
     }
     deleteItem(index){
-        // this.setState((prevState)=>{
-        //     const itemList = prevState.itemList
-        //     itemList.splice(index, 1)
-        //     return {itemList}
-        // })
-        // return el.style.textDecoration= 'line-through'
         const action = deleteItemAction(index)
         store.dispatch(action)
     }
---
+++ src/TodoListUi.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Input, List} from "antd";
+const { Search } = Input;
+const TodoListUi = (props) => {
+		return (
+				<div>
+						<Search value={props.inputValue} onChange={props.handleInput} enterButton="添加" onSearch={props.addItem} placeholder="输入事项" allowClear={true} size='default' style={{width: '300px',margin: '40px auto',display: 'block'}}/>
+						<List
+								bordered
+								dataSource={props.itemList}
+								style={{width: '300px', margin: '-20px auto'}}
+								renderItem={(item, index) => (
+										<List.Item  onClick={(index)=>props.deleteItem(index)}>
+												{item}
+										</List.Item>
+								)}
+						/>
+				</div>
+		)
+}
+export default TodoListUi
